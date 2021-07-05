a7aa0eb5450e4f51bde5285dd5a767a8bf72a751
rezaizadijahromi <rezaizadij2000@gmail.com>
2021-07-06 00:00:01 +0430

    New solutions

---
+++ contest/9595/solution.py
@@ -0,0 +1,12 @@
+def diffrence():
+      
+    n = int(input())
+    count = 0
+    for i in range(n):
+        a = input().replace(' ', '')
+        b = input().replace(' ', '')
+        if a != b:
+            count += 1
+    return count
+
+print(diffrence())
