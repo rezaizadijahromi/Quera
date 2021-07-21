aec579ac6be9b863c7819eae76b4c98925697c56
rezaizadijahromi <rezaizadij2000@gmail.com>
2021-07-21 13:47:55 +0430

    Add 2885

---
+++ contest/cpp/2885/2885.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i;
+    cin >> i;
+
+    for(int j=0; j < i; i++){
+        cout << "man khoshghlab hastam";
+    }
+    return 0;
+}
