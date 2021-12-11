4afaa450e0b23b04ec2d79fe5090f9ae6290b513
rezaizadijahromi <rezaizadij2000@gmail.com>
2021-12-11 20:11:39 +0330

    add solution for 6402

---
+++ contest/cpp/6402/6402.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main(){
+
+    int n;
+    // get the number of charracters
+    cin >> n;
+
+    vector<string> names(n);
+
+    for(int i=0; i < n; i++){
+        cin >> names[i];
+    }
+
+    for(int i=0; i < names.size() - 1; i++){
+        cout << names[i] << " to " << names[i + 1] << ": ke ba in dar agar dar bande dar manand, dar manand." << endl;
+
+        for (int j = i + 1; j > 0; --j)
+        {
+            cout << names[j] << " to " << names[j - 1] << ": dar manand?" << endl;
+        }
+
+        for (int j = 0; j <= i; ++j)
+        {
+            cout << names[j] << " to " << names[j + 1] << ": dar manand." << endl;
+        }
+    }
+}
