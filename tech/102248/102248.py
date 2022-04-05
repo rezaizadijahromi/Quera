cd55e0532a7d0feb79dedcbf9731b4a5ea1cf44e
rezaizadijahromi <rezaizadij2000@gmail.com>
2022-04-05 19:24:51 +0430

    solve 87176 and 102248 quera tech competion

---
+++ tech/102248/102248.py
@@ -0,0 +1,31 @@
+from operator import le
+
+
+def compare(string1, string2):
+    while string1 is not None or string2 is not None:
+        s1, s2 = string1[0], string2[0]
+        if ord(s1) > ord(s2):
+            string2 = string2[1::]
+        elif ord(s1) < ord(s2):
+            string1 = string1[1::]
+        elif ord(s1) == ord(s2):
+            string1 = string1[1::]
+            string2 = string2[1::]
+
+        # print(string1)
+        # print(string2)
+        if len(string1) == 0 and len(string2) == 0:
+            return "Both strings are empty!"
+
+        if len(string1) == 0 or len(string2) == 0:
+            if len(string1) == 0:
+                return string2
+            else:
+                return string1
+
+        string1 = string1[::-1]
+        string2 = string2[::-1]
+
+
+# print(compare('amin', 'nima'))
+# compare('ali', 'salib')
