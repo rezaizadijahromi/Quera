48d09c955d217565f3bd2180030323bfd3848e63
reza <rezaizadij2000@gmail.com>
2022-10-01 08:44:10 +0330

    create essentials for quera 148637 question

--- contest/rust/quera148637/src/main.rs
+++ contest/rust/quera148637/src/main.rs
@@ -1,3 +1,22 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let mut buffer = String::new();
+    io::stdin().read_line(&mut buffer).unwrap();
+
+    let airplanes_bounds: Vec<&str> = buffer.trim().split(' ').collect();
+
+    let number_of_airplanes: i32 = airplanes_bounds[0].trim().parse().unwrap();
+    let number_of_air_bounds: i32 = airplanes_bounds[1].trim().parse().unwrap();
+
+    let mut airplanes: Vec<String> = Vec::new();
+    for _ in 0..number_of_airplanes{
+        let mut airplane: String = String::new();
+        io::stdin().read_line(&mut airplane).unwrap();
+        airplanes.push(airplane);
+    }
+
+    let mut commands: String = String::new();
+    io::stdin().read_line(& mut commands).unwrap();
+
 }
---
+++ contest/rust/quera148637/target/rls/debug/deps/quera148637-6609b38670bbc6ec.d
@@ -0,0 +1,5 @@
+d:\dj\Quera\contest\rust\quera148637\target\rls\debug\deps\quera148637-6609b38670bbc6ec.rmeta: src\main.rs
+
+d:\dj\Quera\contest\rust\quera148637\target\rls\debug\deps\quera148637-6609b38670bbc6ec.d: src\main.rs
+
+src\main.rs:
