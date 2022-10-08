d194bccc487181b7b10dd6f6d7e2888efcbbef46
reza <rezaizadij2000@gmail.com>
2022-10-08 11:43:29 +0330

    solve a simple question

---
+++ contest/rust/quera140039/src/main.rs
@@ -0,0 +1,10 @@
+use std::io;
+
+
+fn main() {
+    let mut inputs: String = String::new();
+    io::stdin().read_line(&mut inputs).expect("Some thing went wrong in inputs");
+
+    println!("quera!");
+
+}
